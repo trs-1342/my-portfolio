8b94b477028fe2093f166c4e13abe101c3af5564
trs-1342 <socialmediaoftrs1342@gmail.com>
2025-10-12 07:32:42 +0300

    responsive nav menu and add icones

--- components/Nav.js
+++ components/Nav.js
@@ -5,12 +5,12 @@ import { useEffect, useRef, useState } from "react";
 
 const items = [
   { href: "/", label: "Anasayfa", icon: "fa-solid fa-house" },
-  { href: "/my-projects", label: "Projelerim", icon: "fa-solid fa-grid-2" },
+  { href: "/my-projects", label: "Projelerim", icon: "fa-solid fa-diagram-project" },
   { href: "/about", label: "Hakkımda", icon: "fa-solid fa-user" },
   {
     href: "/thanks",
     label: "Teşekkürler",
-    icon: "fa-solid fa-hands-holding-heart",
+    icon: "fa-solid fa-heart", // Yeni ikon eklendi
   },
 ];
 
