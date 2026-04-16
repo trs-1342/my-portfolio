f54d5c4af5113950a46d04ffc1a02ca72ae40bd2
trs-1342 <socialmediaoftrs1342@gmail.com>
2026-04-16 21:32:52 +0300

    test

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey:            process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain:        process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId:         process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket:     process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  storageBucket:     process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET | "my-portfolio-1342.appspot.com",
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId:             process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
